8ba64bab15f255f987157a0cceb8e03906f40185
anilsanto <santoanil@gmail.com>
2014-03-20 15:34:09 +0530

    Initial Commit

---
+++ Game/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Game
+//
+//  Created by Nuevalgo on 20/03/14.
+//  Copyright (c) 2014 Nuevalgo. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
